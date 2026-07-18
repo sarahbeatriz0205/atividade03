d761e653216bc760546f70b48f51b2730d2d6e0a
Thiago Gonsalves <geekland135@gmail.com>
2026-07-18 15:28:30 -0300

    Questões C e D creio estarem ok

---
+++ scripts/comandos_juncoes.sql
@@ -0,0 +1,43 @@
+-- Comando D1.
+-- Exibir as publicações juntamente com o nome do usuário que as realizou.
+SELECT
+    U.nome,
+    P.descricao,
+    P.data
+FROM
+    Usuario AS U
+    INNER JOIN Publicacao AS P ON (U.usuario = P.usuario);
+
+-- Comando D2.
+-- Exibir os comentários, o usuário que comentou e a publicação correspondente.
+SELECT
+    U.nome,
+    P.descricao,
+    C.texto,
+    C.data
+FROM 
+    Usuario AS U
+    INNER JOIN Comentario AS C ON (U.usuario = C.usuario)
+    INNER JOIN Publicacao AS P ON (C.idPublicacao = P.id);
+
+-- Comando D3.
+-- Listar todos os usuários, incluindo aqueles que ainda não possuem perfil cadastrado.
+SELECT
+    U.usuario,
+    U.nome,
+    P.bio,
+    P.avatar
+FROM 
+    Usuario AS U
+    LEFT OUTER JOIN Perfil AS P ON (U.usuario = P.usuario);
+
+-- Comando D4.
+-- Exibir todos os usuários e seus perfis, incluindo registros sem correspondência entre as tabelas.
+SELECT
+    U.usuario,
+    U.nome,
+    P.bio,
+    P.genero
+FROM 
+    Usuario AS U
+    FULL OUTER JOIN Perfil AS P ON (U.usuario = P.usuario);
---
+++ scripts/comandos_select.sql
@@ -0,0 +1,42 @@
+-- Comando C1.
+-- Listar todos os usuários cadastrados na plataforma.
+SELECT *
+FROM Usuario;
+
+-- Comando C2.
+-- Listar as publicações realizadas por um usuário específico.
+SELECT
+    id,
+    descricao,
+    data
+FROM Publicacao
+WHERE usuario = 'Sarah';
+
+-- Comando C3.
+-- Listar os comentarios de uma publicação com data maior ou igual a 01/01/2026
+SELECT
+    *
+FROM Comentario
+WHERE 
+    idPublicacao = 1
+    AND data >= '2026-01-01';
+
+-- Comando C4.
+-- Exibir a quantidade de publicações realizadas por cada usuário.
+SELECT
+    usuario,
+    COUNT(*) AS quantidade_publicacoes
+FROM Publicacao
+GROUP BY usuario
+HAVING COUNT(*) >= 1;
+
+-- Comando C5.
+-- Listar os usuários que já realizaram pelo menos uma publicação.
+SELECT
+    usuario,
+    nome
+FROM Usuario
+WHERE usuario IN (
+    SELECT usuario
+    FROM Publicacao
+);
